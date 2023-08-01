a3e576fab97cc0dadabab5a22e53653bf3aadbbc
Chandan Gond <125117082+ChandanGond@users.noreply.github.com>
2023-08-01 16:58:34 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-//Question: Write a Java program to create a class called Shape with a method called 
-//getArea(). Create a subclass called Rectangle that overrides the getArea() 
-//method to calculate the area of a rectangle.
+/* Question: Write a Java program to create a class called Shape with a method called 
+getArea(). Create a subclass called Rectangle that overrides the getArea() 
+method to calculate the area of a rectangle. */
 
 
 class Shape
