3771ef4874424eb4e842b5349919de57559cdf35
Chandan Gond <125117082+ChandanGond@users.noreply.github.com>
2023-08-01 17:09:10 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,35 +1,51 @@
-/* Question: Write a Java program to create a class called Shape with a method called 
-getArea(). Create a subclass called Rectangle that overrides the getArea() 
-method to calculate the area of a rectangle. */
+/* Write a Java program to create a class called Employee with methods called work() 
+and getSalary(). Create a subclass called HRManager that overrides the work()
+method and adds a new method called addEmployee(). */
 
 
-class Shape
-{
-	public double getArea()
-	{
-		return 0.0;
-	}
-}
-class Rectangle  extends Shape
-{
-	private int length; private int width;
-	
-	public Rectangle(int length, int width)
-	{
-		this.length = length;
-		this.width = width;
-	}
-	public double getArea()
-	{
-		return length*width;
-	}
+class Employee {
+    private int salary;
+
+    public Employee(int salary) {
+        this.salary = salary;
+    }
+
+    public void work() {
+        System.out.println("working as an employee!");
+    }
+
+    public int getSalary() {
+        return salary;
+    }
 }
 
-public class Main
-{
-	public static void main(String[] args)
-	{
-		Rectangle rectangle = new Rectangle(12,12);
-		double area = rectangle.getArea();
-	    System.out.println("The area of the rectangle is: " + area);	}
+class HRManager extends Employee {
+ public HRManager(int salary) {
+     super(salary);
+ }
+
+ public void work() {
+     System.out.println("\nManaging employees");
+ }
+
+ public void addEmployee() {
+     System.out.println("\nAdding new employee!");
+ }
+}
+
+
+//Main.java
+//Main class
+public class Main {
+ public static void main(String[] args) {
+     Employee emp = new Employee(40000);
+     HRManager mgr = new HRManager(70000);
+
+     emp.work();
+     System.out.println("Employee salary: " + emp.getSalary());
+
+     mgr.work();
+     System.out.println("Manager salary: " + mgr.getSalary());
+     mgr.addEmployee();
+ }
 }
